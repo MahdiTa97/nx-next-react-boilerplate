cf816634e94982a9b4f7b522a160677f3ce4a8ac
MahdiTa97 <taala.mahdy@gmail.com>
2022-12-26 19:30:00 +0330

    feat: add tailwind eslint config

--- libs/ui/src/lib/ui.tsx
+++ libs/ui/src/lib/ui.tsx
@@ -1,14 +1,11 @@
-import styles from './ui.module.css'
+import styles from './ui.module.css';
 
-/* eslint-disable-next-line */
-export interface UiProps {}
-
-export function Ui(props: UiProps) {
+export function Ui() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Ui!</h1>
+      <h1 className="m-5 flex">Welcome to Ui!</h1>
     </div>
-  )
+  );
 }
 
-export default Ui
+export default Ui;
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 const Configuration = {
-  '{packages,libs}/**/*.{ts,js,json,md,css,scss,html}': [
+  '{packages,libs}/**/*.{ts,js,json,md,css,scss,html,tsx,jsx}': [
     `nx affected --target lint --uncommitted --fix true`,
     'nx affected --target test --uncommitted',
     'nx lint --uncommitted',
   ],
-}
+};
 
-module.exports = Configuration
+module.exports = Configuration;
